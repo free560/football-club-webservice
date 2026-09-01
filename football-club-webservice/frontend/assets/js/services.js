6d73b994c28883bc707394813d47931ea2cddd72
Negos560 <negosfree@gmail.com>
2026-09-01 03:19:38 -0700

    Update services.js

--- football-club-webservice/frontend/assets/js/services.js
+++ football-club-webservice/frontend/assets/js/services.js
@@ -254,7 +254,7 @@ playerForm?.addEventListener("submit", async (e) => {
     };
 
     try {
-        await fetch("VOTRE_URL_GOOGLE_APPS_SCRIPT", {
+        await fetch("https://script.google.com/macros/s/AKfycbwCasgkAu0KmoHn1MIPEXXb1x_sN2Ud5QCAhsd2OrK7HHqpfOf3UcyLFzzcoewRI_zb/exec", {
             method: "POST",
             body: JSON.stringify(data)
         });
